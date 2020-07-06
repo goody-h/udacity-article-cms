e59d8a39e08276fabb70965ee06ded6f87bd2c08
Goodhope Ordu <goodhopeordu@yahoo.com>
2020-07-06 10:07:08 +0000

    fix logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 logging.basicConfig(
-    filename='./flask-app.log',
-    filemode='a',
+    # filename='./flask-app.log',
+    # filemode='a',
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
